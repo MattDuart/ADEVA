db298331e0f7ea024bf7bfa1bc87bb45126b2c4f
MattDuart <matt_dev@outlook.com.br>
2023-06-12 10:31:05 -0300

    deploy 1

---
+++ adeva_project/s_settings.py
@@ -0,0 +1,19 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6gqzmftz7%7iu!bo=2110yonrwtzk)0m4sq^@7n)v5(@5_wf-s'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'giaadeva_adeva',
+        'USER': 'giaadeva_system ',
+        'PASSWORD': 'jvk9#eTC#j!j',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
